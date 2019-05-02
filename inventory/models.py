1002be10fa21ceff4c17f1e2ed30996f6518c61f
Harshit Garg <harshitgargmnit@gmail.com>
2019-05-03 01:23:35 +0530

    Update settings.py for differnet environments
    Note: Ensure environment file is properly configured

--- inventory/models.py
+++ inventory/models.py
@@ -47,3 +47,27 @@ class Product(models.Model):
             "vendor": self.vendor.json,
             "category": self.category.json,
         }
+
+
+class PersonQuerySet(models.QuerySet):
+    def authors(self):
+        return self.filter(role='A')
+
+    def editors(self):
+        return self.filter(role='E')
+
+class PersonManager(models.Manager):
+    def get_queryset(self):
+        return PersonQuerySet(self.model, using=self._db)
+
+    def authors(self):
+        return self.get_queryset().authors()
+
+    def editors(self):
+        return self.get_queryset().editors()
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    role = models.CharField(max_length=1)
+    people = PersonManager()
--- just_shoping/wsgi.py
+++ just_shoping/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "just_shoping.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "just_shoping.settings.production")
 
 application = get_wsgi_application()
